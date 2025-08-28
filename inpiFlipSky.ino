b105ada8f268dd6fbc75da6e88d6644ada5d02da
unknown <boris.vargas@intech-automacao.com.br>
2025-08-28 18:33:07 -0300

    Refatoração e comentários

--- inpiFlipSky.ino
+++ inpiFlipSky.ino
@@ -1,25 +1,42 @@
-/*
-
-*/
-
-#include "FlipskyPPM.h"
-#include <Servo.h> // Include the Servo library
-
-FlipskyPPM reciever(2);
-Servo myservo; // Create a Servo object named myservo
-
-#define in45g 12
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Firmware para controle de servomotor com base no controle
+//remoto via LORAWAN FlipSky VX3 via controle PPM e não 
+//UART
+//Autor: Boris Vargas 2025
+// Library: Flipsky Remote Reader Library
+//https://github.com/Chipney/FlipskyPPM
+//Créditos para: Aaron Beckmann 2021
+//License: GNU GPL v3 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include "FlipskyPPM.h" // Flipsky library
+#include <Servo.h> // Inclui Servo library
+//Instance class
+FlipskyPPM reciever(2);//Receiver PPM ligado no PINO 2 do Arduino pro-mini
+Servo servoMotor; 
+//Seleção do curso em graus com base nas entradas digitais
+#define in45g 12 
 #define in90g 10
 #define in120g 11
-#define chave 4
-#define partida 5
-int servoPos = 0; // Variable to store the servo position
-
+//Saídas digitais
+#define chave 4 // Chave que liga geral
+#define partida 5 // Motor de partida
+#define statusLed 13 //Led da propria placa arduino para indicar fora de range o controle.
+//Variaveis
+int servoPos = 0; // Posição do servo
+int controlValue = 0;
+int posSelected = 180;
+unsigned long previousMillis = 0;
+const long interval = 2000;
+bool fpPulseLigaDesliga = false;
+bool modoPartidaAtivo = false;
+bool jetLigado = false;
+bool transicaoLigaDesliga = false;
+//Init Setup
 void setup()
 {
   Serial.begin(115200);
-  myservo.attach(9); // Attaches the servo on pin 9 to the Servo object
-  pinMode(13,OUTPUT);
+  servoMotor.attach(9); //Atribui pino 9 para PWM do servo
+  pinMode(statusLed,OUTPUT);
   pinMode(chave,OUTPUT);
   digitalWrite(chave,LOW);
   pinMode(partida,OUTPUT);
@@ -27,25 +44,13 @@ void setup()
   pinMode(in45g,INPUT_PULLUP);
   pinMode(in90g,INPUT_PULLUP);
   pinMode(in120g,INPUT_PULLUP);
-  Serial.println("Init FlyControl siroB@");
 }
-
-int controlValue = 0;
-int posSelected = 180;
-
-unsigned long previousMillis = 0;
-const long interval = 2000;
-
-bool fpPulseLigaDesliga = false;
-bool modoPartidaAtivo = false;
-bool jetLigado = false;
-bool transicaoLigaDesliga = false;
-
+//Main Loop
 void loop(){
-   unsigned long currentMillis = millis();
-
-  controlValue = reciever.getNewestValue(); //Flipsky Remotes send new value every ~20ms
-
+  unsigned long currentMillis = millis();
+  controlValue = reciever.getNewestValue(); //Flipsky envia novo valor a cada ~20ms
+  //Apertando o freio do controle os valores ficam entre 0 e -20
+  //Neste caso este modo é usado para ligar/desliga e acionar motor de partida
   if(controlValue<0 and controlValue>-20){
     modoPartidaAtivo = true;
   }else{
@@ -62,12 +67,11 @@ void loop(){
   }
 
   if(modoPartidaAtivo and (currentMillis - previousMillis >= interval) and !jetLigado and !transicaoLigaDesliga){
-      Serial.println("Modo de liga");
-      transicaoLigaDesliga = true;
-      digitalWrite(chave, HIGH);
-      jetLigado = true;
-      delay(500);
-      digitalWrite(partida, HIGH);
+    transicaoLigaDesliga = true;
+    digitalWrite(chave, HIGH);
+    jetLigado = true;
+    delay(500);
+    digitalWrite(partida, HIGH);
   }
 
   if(!modoPartidaAtivo and jetLigado){
@@ -75,7 +79,6 @@ void loop(){
   }
 
   if(modoPartidaAtivo and (currentMillis - previousMillis >= interval) and jetLigado and !transicaoLigaDesliga){
-    Serial.println("Desliguei JET");
     transicaoLigaDesliga = true;
     jetLigado = false;
     digitalWrite(chave, LOW);
@@ -97,13 +100,13 @@ void loop(){
     posSelected = 180;
   }
   if (controlValue < -100 || controlValue > 1000){
-    digitalWrite(13, !digitalRead(13));
+    digitalWrite(statusLed, !digitalRead(statusLed));
   }else{
-    digitalWrite(13, LOW);
+    digitalWrite(statusLed, LOW);
   }
 
   servoPos = map(controlValue,480,956,0,posSelected);
-  myservo.write(servoPos);
+  servoMotor.write(servoPos);
   delay(1); 
   
 }
